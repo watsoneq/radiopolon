bb0fc289f3a2af1e582b212dc20c03745bf423a7
Antek Majewski <antos.majewski@wp.pl>
2025-04-10 20:37:20 +0200

    Initial commit from FluttLab

---
+++ android/app/src/main/kotlin/com/watsonekk/radiopolon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.watsonekk.radiopolon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Color redAccent = Color(0xFFFF3B30);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Radio Polon',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+          foregroundColor: redAccent,
+        ),
+        iconTheme: IconThemeData(color: redAccent),
+      ),
+      home: RadioPolon(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class RadioPlayer extends StatefulWidget {
+  @override
+  _RadioPlayerState createState() => _RadioPlayerState();
+}
+
+class _RadioPlayerState extends State<RadioPlayer> {
+  late AudioPlayer _player;
+
+  @override
+  void initState() {
+    super.initState();
+    _player = AudioPlayer();
+    _player.setUrl('https://stream.zeno.fm/cn78er2mewmtv');
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final redAccent = Color(0xFFFF3B30);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Radio Polon')),
+      body: Center(
+        child: StreamBuilder<PlayerState>(
+          stream: _player.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            final playing = playerState?.playing ?? false;
+            final processing = playerState?.processingState;
+
+            if (processing == ProcessingState.loading ||
+                processing == ProcessingState.buffering) {
+              return CircularProgressIndicator(color: redAccent);
+            } else if (!playing) {
+              return IconButton(
+                icon: Icon(Icons.play_arrow),
+                iconSize: 80,
+                color: redAccent,
+                onPressed: _player.play,
+              );
+            } else {
+              return IconButton(
+                icon: Icon(Icons.pause),
+                iconSize: 80,
+                color: redAccent,
+                onPressed: _player.pause,
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
